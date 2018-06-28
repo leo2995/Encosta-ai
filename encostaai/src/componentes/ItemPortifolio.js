ca5e98fea912d4c65aa0ed6dbb6294c45b5e1b76
lsg <lsg@MOREIRAO.local>
2018-06-27 21:56:18 -0300

    butao

--- encostaai/src/componentes/ItemPortifolio.js
+++ encostaai/src/componentes/ItemPortifolio.js
@@ -1,26 +1,35 @@
 import React from 'react'
 
+import {config, database} from '../firebase-Config'
+
+
 const ItemPortfolio = props => {
     return (
         <div className='col-sm-4'>
             <div className='thumbnail'>
                 <img src='paris.jpg' alt='' width='400' height='300' />
                 <h4>Estacionamento Privado</h4>
-                <p><label>Vagas Diponiveis:</label> <strong>{props.conteudo.emptySpots}</strong></p>
+                <p> <label>Vagas Diponiveis:</label> <strong>{props.conteudo.emptySpots}</strong></p>
                 <p><label>Avaliações:</label> <strong>{props.conteudo.rating}</strong></p>
                 <p><strong>{props.conteudo.name}</strong></p>
                 <p><label>Vagas Totais:</label> <strong>{props.conteudo.spaceQnt}</strong></p>
+
                 <p><input type="button" value="Ocupar vaga" onClick={
                     function () {
-                        if (props.conteudo.emptySpots > 0)
-                            props.conteudo.emptySpots = props.conteudo.emptySpots - 1;
+                        var updates = ++props.conteudo.emptySpots
+                        var result = {emptySpots:updates.toString()}
+                        var chave = props.chave
+                        return database.ref('privateParking').child(chave).update(result)
                     }
                 }/></p>
                 <p><input type="button" value="Desocupar vaga" onClick={
+                    
                     function () {
-                        if (props.conteudo.emptySpots < props.conteudo.spaceQnt) {
-                            props.conteudo.emptySpots = props.conteudo.emptySpots + 1;
-                        }
+                        var updates = --props.conteudo.emptySpots
+                        var result = {emptySpots:updates.toString()}
+                        var chave = props.chave
+                        return database.ref('privateParking').child(chave).update(result)
+                        
                     }
                 }/></p>
 
