6ce3c70b7d35463c52a88f31062dcf9f31c1b760
lsg <lsg@MOREIRAO.local>
2018-06-27 22:03:30 -0300

    butoes retirados

--- encostaai/src/componentes/StreetParking.js
+++ encostaai/src/componentes/StreetParking.js
@@ -11,17 +11,6 @@ const StreetParkink = props=>{
             <p><label>Avaliações:</label> <strong>{props.conteudoPublico.rating}</strong></p>
             <p><strong>{props.conteudoPublico.name}</strong></p>
             <p><label>Vagas Totais:</label> <strong>{props.conteudoPublico.spaceQnt}</strong></p>
-            <p><input type="button" value="Ocupar vaga" onClick={
-                function () {
-                    if (props.conteudoPublico.spaceQnt > 0)
-                        props.conteudoPublico.spaceQnt = props.conteudoPublico.spaceQnt - 1;
-                }
-            }/></p>
-            <p><input type="button" value="Desocupar vaga" onClick={
-                function () {
-                    props.conteudoPublico.spaceQnt = props.conteudoPublico.spaceQnt + 1;
-                }
-            }/></p>
 
         </div>
     </div>
